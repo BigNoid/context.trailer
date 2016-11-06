2b2ef541b5201cc9164d678d0c944c03979b916d
BigNoid <big.noid@kodi.tv>
2016-11-06 15:41:57 +0100

    c/p ftw

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015 Philipp Temminghoff
+# Copyright (C) 2016 BigNoid
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
